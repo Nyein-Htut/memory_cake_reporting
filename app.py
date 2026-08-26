3af1aea8e1a84a6a444263ebb1abaaee4f39d43d
Irene <manyeinhtuttin@gmail.com>
2026-08-26 20:03:52 +0630

    Update app.py

--- app.py
+++ app.py
@@ -73,7 +73,8 @@ class Order(db.Model):
     delivery_fee = db.Column(db.Text, default='')
     is_paid = db.Column(db.Boolean, nullable=False, default=False)
     payment_date = db.Column(db.String(50), default='')
-
+    is_delivered = db.Column(db.Boolean, nullable=False, default=False)  
+    is_folded = db.Column(db.Boolean, nullable=False, default=False)  
     items = db.relationship(
         'OrderItem',
         backref='order',
